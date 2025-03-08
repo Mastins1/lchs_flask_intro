690835bc6dfe91d1fb9dbfc25cc85cbe541b6c0a
spencer mastin <smastin@live.com>
2025-03-07 23:41:57 -0500

    Check Input Length

--- hexcode.py
+++ hexcode.py
@@ -9,7 +9,20 @@ app.config['DEBUG'] = True
 def hex_form():
    if request.method == 'POST':
       hex = request.form['hex']
-      feedback = "Congratulations!"
+      # Remove the '#' symbol if it exists
+      
+      if hex and hex.startswith('#'):
+            hex = hex[1:]       
+      if len(hex) != 6:
+         feedback = "Hex code must contain exactly 6 characters."
+         hex = '00FF00'
+      else:            
+         if all(c in '0123456789ABCDEF' for c in hex.upper()):
+               feedback = "Congratulations"              
+         else:
+               feedback = "Invalid hex color! Please use only digits (0-9) and letters (A-F)."
+               hex = 'FF0000' 
+
    else:
       hex = '987654'
       feedback = "Display here..."
